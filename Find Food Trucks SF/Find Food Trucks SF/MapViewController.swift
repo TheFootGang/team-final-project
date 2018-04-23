cafed3bdcecaa3cba0945cc977cf43c62abd0e29
Mark <merksmanship@gmail.com>
2018-04-22 18:55:54 -0700

    added searchbar and fixed mapview to be responsive with landscape mode

---
+++ Find Food Trucks SF/Find Food Trucks SF/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewController.swift
+//  Find Food Trucks SF
+//
+//  Created by XCode on 4/22/18.
+//  Copyright © 2018 TheFootGang. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+
+    
+}
+    
+
