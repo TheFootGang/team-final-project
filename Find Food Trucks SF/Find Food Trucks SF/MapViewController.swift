85ed475a5a5c1504769bff77db1c1bb4e3e619c8
Stanley <Stanleyliu15@gmail.com>
2018-04-23 13:43:51 -0700

    fix indentation and remove search bar

--- Find Food Trucks SF/Find Food Trucks SF/MapViewController.swift
+++ Find Food Trucks SF/Find Food Trucks SF/MapViewController.swift
@@ -10,10 +10,9 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController,UISearchBarDelegate, CLLocationManagerDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    //@IBOutlet weak var searchBar: UISearchBar!
 
     let service: FoodTruckService = FoodTruckService()
     
@@ -29,34 +28,24 @@ class MapViewController: UIViewController,UISearchBarDelegate, CLLocationManager
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
     }
+    
     override func viewWillAppear(_ animated: Bool) {
-                
         if self.region != nil {
             mapView.setRegion(region, animated: true)
         }
-        
         addMapAnnotations()
-        
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
-        
         let span: MKCoordinateSpan = MKCoordinateSpanMake(0.02, 0.02)
         let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
-        self.region = region
-        mapView.setRegion(region, animated: false)
         
+        self.region = region
+        self.mapView.setRegion(region, animated: false)
         self.mapView.showsUserLocation = true
     }
-    
-
     
     private func addMapAnnotations() {
         service.getFoodTrucks() { [unowned self] (foodTrucks: [FoodTruck]?) in
@@ -64,19 +53,17 @@ class MapViewController: UIViewController,UISearchBarDelegate, CLLocationManager
                 print("Error fetching food trucks.")
                 return
             }
+            
             for foodTruck in foodTrucks {
+                let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(foodTruck.latitude, foodTruck.longitude)
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate
+                annotation.title = foodTruck.name
+                annotation.subtitle = foodTruck.daysHours
                 
-                    let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(foodTruck.latitude, foodTruck.longitude)
-                    let annotation = MKPointAnnotation()
-                    annotation.coordinate = location
-                    annotation.title = foodTruck.name
-                    annotation.subtitle = foodTruck.daysHours
-                    self.mapView.addAnnotation(annotation)
-                    //print(foodTruck.latitude)
+                self.mapView.addAnnotation(annotation)
             }
-        
         }
-    
     }
     
 }
