337514ed46275436600325cbfcd3030b5e345cef
Brandonyu <beyu94@gmail.com>
2018-05-11 19:49:06 -0700

    Fixing issue with commits on Github not showing my proper username contributions.

--- Find Food Trucks SF/Find Food Trucks SF/MapViewController.swift
+++ Find Food Trucks SF/Find Food Trucks SF/MapViewController.swift
@@ -65,7 +65,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         let region: MKCoordinateRegion = MKCoordinateRegionMake(coordinates, span)
         self.mapView.setRegion(region, animated: false)
     }
-    //test
+
+    
     private func addMapAnnotations() {
         service.getFoodTrucks() { [unowned self] (foodTrucks: [FoodTruck]?) in
             guard let foodTrucks = foodTrucks else {
@@ -77,12 +78,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             
             for foodTruck in self.foodTrucks {
                 let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(foodTruck.latitude, foodTruck.longitude)
+                
                 let annotation = FoodTruckAnnotation(id: foodTruck.id, title: foodTruck.name, subtitle: foodTruck.daysHours, coordinate)
+                
                 self.mapView.addAnnotation(annotation)
             }
         }
     }
 }
+
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
