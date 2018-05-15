618bf2d605e3c9e89bf215017b0ce1d11397b96f
Stanley <Stanleyliu15@gmail.com>
2018-05-15 12:23:35 -0700

    remove unnecessary call to view will appear

--- Find Food Trucks SF/Find Food Trucks SF/BookmarksViewController.swift
+++ Find Food Trucks SF/Find Food Trucks SF/BookmarksViewController.swift
@@ -38,10 +38,6 @@ class BookmarksViewController: UIViewController, UITableViewDataSource, UITableV
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
-    }
     override func viewDidAppear(_ animated: Bool) {
         self.bookmarks.removeAll()
         viewDidLoad()
